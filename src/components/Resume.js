7d4cc60ea174a3d580b48f3fec71b30fb8623a49
Uvais <sheikhuvais289@gmail.com>
2023-12-02 20:08:10 +0530

    My_Portfolio

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -6,7 +6,7 @@ const Resume = () => {
     <div id="resume">
       <h2>My Resume</h2>
       <p>Click the link below to view and download my resume:</p>
-      <a href="https://drive.google.com/file/d/1aOYVTn2zpk-zEc1BmNi9pH9dwVMb5Z5s/view?usp=drive_link" download="YourResumeFileName.pdf">
+      <a href="https://drive.google.com/file/d/1nkilDZJcgCTcBcIorofx8uj23ituqPrt/view?usp=drive_link" download="YourResumeFileName.pdf">
         Download Resume
       </a>
     </div>
